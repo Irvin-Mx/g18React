c7b8c1b47fa5d8885a467cec0dd01e98000df518
Irvin Martinez <irvin0329@outlook.com>
2022-06-15 20:57:40 -0600

    ejercicio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+//Components
+import Title from "./components/Title";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const [light, setLight] = useState(false);
+
+  const handleClick = (increase) => {
+    let newCount = count;
+
+    if (increase) {
+      newCount = newCount + 1;
+    } else {
+      newCount = newCount - 1;
+    }
+    setCount(newCount);
+    setLight(!light);
+    
+  };
+
+  //Argumento 1: Callback
+  //Argumento 2: nada, [], [count]
+  //Caso vacío: el call back del useEffect se ejecuta cada que algo cambie (estado)
+  //Caso []: el callback del useEffect se ejecuta la primera vez que carga el componente
+  //Caso [count]: el callback del useEffect se ejecuta la primera vez el estado se actualiza
+  useEffect( () =>{
+    
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title firstName="Peter" lastName="Parker"></Title>
+        <Title firstName="Rick" lastName="Sanchez"></Title>
+        <h1>{count}</h1>
+        <button onClick={() => handleClick(true)}>Click me to add</button>
+        <button onClick={() => handleClick(false)}>Click me to reduce</button>
+
+        <div className={light === true ? "on": "off"}>Off</div>
       </header>
     </div>
   );
---
+++ src/components/Title/index.jsx
@@ -0,0 +1,11 @@
+
+
+
+//Styles
+import './Title.css'
+
+
+export default function Title(props){
+
+    return <h1>{props.firstName} {props.lastName}</h1>
+}
