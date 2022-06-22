5acac083f901a5ff52da2f5f8ab56941be3937ff
Irvin Martinez <irvin0329@outlook.com>
2022-06-21 19:13:50 -0600

    form created

--- src/App.js
+++ src/App.js
@@ -1,143 +1,108 @@
-import { useEffect, useState } from "react";
+// Components
+import Card from "./components/Card";
 
-//Components
-import Title from "./components/Title";
-
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const koders = [
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [photoURL, setPhotoURL] = useState("");
+
+  const [mxn, setMxn] = useState(0)
+
+  const [koders, setKoders] = useState([
     {
       firstName: "Luis",
       lastName: "Vera",
       age: 24,
       gender: "m",
-      photoURL: "URL VALIDA",
-      email: "luisvera@gmail.com",
-    },
-    {
-      firstName: "Nestor",
-      lastName: "Ramírez",
-      age: 40,
-      gender: "m",
-      photoURL: "URL VALIDA",
-      email: "nestorramirez@gmail.com",
-    },
-    {
-      firstName: "David",
-      lastName: "Romero",
-      age: 28,
-      gender: "m",
-      photoURL: "URL VALIDA",
-      email: "davidromero@gmail.com",
+      photoURL: "https://sgame.etsisi.upm.es/pictures/18253.jpg?1621958969/",
     },
     {
       firstName: "Yusef",
       lastName: "Lopéz",
       age: 40,
-      gender: "m",
-      photoURL: "URL VALIDA",
-      email: "yuseflopez@gmail.com",
+      gender: "f",
+      photoURL: "https://media4.giphy.com/media/5sYyfIMRcpJWNqdySh/giphy.gif",
     },
-  ];
+  ]);
 
-  const animals = ["perro", "gato", "oso", "tortuga"];
-
-  const animalsUI = animals.map((animal, index) => (
-    <li key={index}> {animal} </li>
-  ));
-
-  const koderUi = koders.map((koder, index) => (
-    <div className="koderUiLi" key={index}>
-      <div className="avatarDiv">
-        <div className="avatarImg">
-          <img alt="koderIcon" src={koder.photoURL} />
-        </div>
-      </div>
-
-      <div className="koderInfo">
-        <div className="koderInfoCentered">
-          {koder.firstName} {koder.lastName}
-        </div>
-        <div className="koderInfoCentered">
-           Age:{koder.age}
-        </div>
-        <div className="koderInfoCentered">
-           Email Adress: {koder.email}
-        </div>
-      </div>
-    </div>
-  ));
-
-  const [count, setCount] = useState(0);
-
-  const [light, setLight] = useState(false);
-
-  const [highLighted, sethighLighted] = useState(false);
-
-  const handleClick = (increase) => {
-    let newCount = count;
-
-    if (increase) {
-      newCount = newCount + 1;
-    } else {
-      newCount = newCount - 1;
+  const kodersUI = koders.map(
+    ({ firstName, lastName, age, gender, photoURL }, index) => {
+      console.log(firstName);
+      return (
+        <Card
+          key={index}
+          photoURL={photoURL}
+          firstName={firstName}
+          lastName={lastName}
+          age={age}
+          gender={gender}
+        />
+      );
     }
-    setCount(newCount);
-    setLight(!light);
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("AGREGAR KODER");
+    const newKoders = [
+      ...koders,
+      {
+        firstName,
+        lastName,
+        age,
+        gender,
+        photoURL,
+      },
+    ];
+
+    setKoders(newKoders);
+
+    setFirstName("");
+    setLastName("");
+    setAge("");
+    setGender("");
+    setPhotoURL("");
   };
 
-  const handleLight = () => {
-    setLight(!light);
-  };
-  const handleLight2 = () => {
-    setLight(!light);
-  };
-
-  //Argumento 1: Callback
-  //Argumento 2: nada, [], [count]
-  //Caso vacío: el call back del useEffect se ejecuta cada que algo cambie (estado)
-  //Caso []: el callback del useEffect se ejecuta la primera vez que carga el componente
-  //Caso [count]: el callback del useEffect se ejecuta la primera vez el estado se actualiza
-  useEffect(() => {});
+  console.log(koders, "KODERS");
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <Title firstName="Peter" lastName="Parker"></Title>
-        <Title firstName="Rick" lastName="Sanchez"></Title>
-        <h1>{count}</h1>
-        <button onClick={() => handleClick(true)}>Click me to add</button>
-        <button onClick={() => handleClick(false)}>Click me to reduce</button>
-
-        <div
-          className={light === true ? "on" : "off"}
-          onMouseOver={() => {
-            handleLight();
-          }}
-          onMouseLeave={() => {
-            handleLight2();
-          }}
-        >
-          {light === true ? "on" : "off"}
-        </div>
-
-        
-      </header> */}
-
-      {/* <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul> */}
-
-      {/* <ul>{animalsUI}</ul> */}
-
-      <div className="koderUl">{koderUi}</div>
+      <div className="main-container">
+        <div className="container">{kodersUI}</div>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+        <input
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+        />
+        <input value={age} onChange={(event) => setAge(event.target.value)} />
+        <input
+          value={gender}
+          onChange={(event) => setGender(event.target.value)}
+        />
+        <input
+          value={photoURL}
+          onChange={(event) => setPhotoURL(event.target.value)}
+        />
+        <button type="submit">Agregar Koder</button>
+      </form>
+      <h1>
+      {isNaN(   (mxn*18).toFixed(2)    ) ? "Numbers only please" : (mxn*18).toFixed(2)}
+      </h1>
+      <input
+          value={mxn}
+          onChange={(event) => setMxn(event.target.value)}
+        />
     </div>
   );
 }
